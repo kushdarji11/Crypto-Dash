32c16059c40ea552aadf75258b16bd1a4d1e980e
kushdarji11 <49285048+kushdarji11@users.noreply.github.com>
2022-01-10 00:19:02 -0500

    Initial Push

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '../styles/globals.css'
 
+import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
